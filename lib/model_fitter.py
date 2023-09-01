09c5c4864de6d034a032bde632eeb900cf7cf621
Milan Zarchev <mzarchev@gmail.com>
2023-09-01 17:18:38 +0200

    Early version of model fitter ready

--- lib/model_fitter.py
+++ lib/model_fitter.py
@@ -1,18 +1,50 @@
 import statsmodels.formula.api as smf 
+from pandas import read_csv
+import marginaleffects as me
 
 class modelFitter:
     def __init__(self):
         self.formula: str = None
-        self.summary = None,
+        self.pred: str    = None
+        self.summary      = None
+        self.fit          = None
+        self.ate          = None
     
     def format_formula(self, pred: str, outc: str, conf=[]):
         # Take a predictor, an outcome and optionally a list of confounders
         # Return a formatted formula string
         formula = f"{outc} ~ {pred}"
         if conf: formula = f"{formula} + {' + '.join(conf)}"
+        self.pred = pred
         self.formula = formula
         
     def fit_model(self, formula, df):
-        fit = smf.logit(formula, data=df).fit()
+        fit = smf.ols(formula, data=df).fit()
+        self.fit = fit
         self.summary = fit.summary()
-        #me.avg_comparisons(fit_sm, variables={"x": [0, 1]})
+        
+    def estimate_ate(self, fit, pred):
+        df_ate = me.avg_comparisons(fit, variables=pred)
+        ate = df_ate.get_column("estimate")[0]
+        ci_low = df_ate.get_column("conf_low")[0]
+        ci_upp = df_ate.get_column("conf_high")[0]
+        p = df_ate.get_column("p_value")[0]
+        
+        ate_fmt = f"{round(ate, 2)} [{round(ci_low, 2)}, {round(ci_upp, 2)}], p={round(p, 3)}"
+        self.ate = ate_fmt
+        
+    def run_fitting(self, pred, outc, data, conf=[]):
+        self.format_formula(pred, outc, conf)
+        self.fit_model(self.formula, data)
+        self.estimate_ate(self.fit, pred)
+
+
+""" model = modelFitter()
+
+df_lbd = read_csv("../df_lbd.csv")
+
+formula = model.format_formula()
+model.run_fitting(pred="Year", outc="Runtime",
+                  data=df_lbd)
+
+model.ate """
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from nicegui import ui, events
 import lib.selected_vars_handler
 import lib.csv_file_handler
-
+import lib.model_fitter
 
 selected_vars_handler = lib.selected_vars_handler.SelectedVarsHandler()
 csv_file_handler = lib.csv_file_handler.CsvFileHandler()
-
+model_fitter = lib.model_fitter.modelFitter()
 
 # Functions
 def respond_to_upload(event: events.UploadEventArguments):
     csv_file_handler.upload_csv(event)
     selected_vars_handler.generate_selects(csv_file_handler.cols)
+ 
+def respond_to_fit_model():
+    model_fitter.run_fitting(pred=selected_vars_handler.pred,
+                             outc=selected_vars_handler.outc,
+                             conf=selected_vars_handler.conf,
+                             data=csv_file_handler.df) 
  
 ### UI design
 with ui.header().classes("h-12 bg-grey"):
@@ -27,18 +33,13 @@ with ui.row():
     
     with ui.card():
         ui.button(text="Fit model",
-                  on_click=lambda: print("clicked")) \
+                  on_click=respond_to_fit_model) \
         .bind_enabled(selected_vars_handler, "specified")
-
-def change_x(event) -> None:
-    x = event.value
-    print(x)
-
-
-ui.select(options = [1, 2, 3],
-          multiple=True,
-          on_change=lambda e: change_x(e)).props("use-chips")
-ui.button(text="test",
-          on_click=lambda: print(f"{selected_vars_handler.outc} --- {selected_vars_handler.pred}"))
+        
+        ui.label() \
+        .bind_text(model_fitter, "ate") \
+        .bind_visibility(model_fitter, "ate")
+        
+        ui.label()
 
 ui.run()
